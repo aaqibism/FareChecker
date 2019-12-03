359f813c76cde279d4aafddea0f6b0a13602c33f
Ahmad Rangeen <rangeen@usc.edu>
2019-12-02 22:21:39 -0800

    Create location.java

---
+++ src/location.java
@@ -0,0 +1,17 @@
+package backend;
+public class location {
+
+	public String name;
+	public double lat;
+	public double lng;
+	public location(String n, double lati,double longi)
+	{
+		this.name= n;
+		this.lat=lati;
+		this.lng=longi;
+		
+	}
+	
+	
+
+}
