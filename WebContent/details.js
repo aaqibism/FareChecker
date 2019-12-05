6862c6b039d8089a20bc0cdf02225ddaa645dd52
Ahmad Rangeen <rangeen@usc.edu>
2019-12-04 18:22:24 -0800

    Update details.js

--- WebContent/details.js
+++ WebContent/details.js
@@ -1,17 +1,26 @@
 function table()
 {
+	var endlat=sessionStorage.getItem("endinglat");
+	var endlng=sessionStorage.getItem("endinglng");
+	var startlat = sessionStorage.getItem("startinglat");
+	var startlng = sessionStorage.getItem("startinglng");   
 	var xhr = new XMLHttpRequest();
+	let origins=startlat+','+startlng+'&destinations='+endlat+'%2C'+endlng;
+	let url= "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+origins+'&key='+API_KEY;
+	console.log(url);
 
+	xhr.open('GET', url,false);
+	xhr.send();
+	var x=JSON.parse(xhr.responseText);
+	console.log(x.rows[0].elements[0].distance.text);
 	xhr.open('POST', "test", false);
 	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xhr.send("startinglat=" + hi + "&endinglat=" + term+"&endinglng="+terms+"&startinglng="+his);
+	xhr.send("distance=" + JSON.stringify(x.rows[0].elements[0].distance.text) );
 	if (xhr.responseText.trim().length > 0) {	
-		console.log("yes");
 		var x=JSON.parse(xhr.responseText);
 		console.log(x[0]);
 		document.getElementById("uber").innerHTML="$"+x[0];
 		document.getElementById("lyft").innerHTML="$"+x[1];
-
-		
 	}
+
 }
